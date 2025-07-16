a45c9ad5d7e83eb142a8eb37c38ba49a7f2e13f7
Jiveshpp1 <parmarjivesh@gmail.com>
2025-07-16 14:05:27 -0400

    edited my python code and added my flowchart and AI chats too

--- python_1/prgect.py
+++ python_1/prgect.py
@@ -15,49 +15,74 @@ else:
     credits = 100
     number_of_plays = 0
     symbles = ['🍒','🍟','🍕','🤓','💎','💵']
+    creditswon = 0
     print('This is a gambling game you have ' + str(credits) + ' credits to use on a bad slot machine')
     bet = input('how much do you wnat to bet 5 or 20 or 10 credits? ')
+    #Function to use slot machine It cheaks if the user has enough credits to play 
     def play(): 
-        global credits, number_of_plays, bet
+        global credits, number_of_plays, bet , creditswon
 
         if bet not in ['1','5', '10','20']:
-            print('You can only bet 1 or 10 credits')
-            play()
-            return
+            print('You can only bet 1 or 10 or 20 credits')
+            quit()
         if credits <= 0:
             print('You have no credits left to play')
             print('You played ' + str(number_of_plays) + ' times')
+            print('You won ' + str(creditswon) + ' credits')
             quit()      
         oof =[random.choice(symbles),random.choice(symbles),random.choice(symbles)]
+        oofu = [random.choice(symbles),random.choice(symbles),random.choice(symbles)]
+        oofd = [random.choice(symbles),random.choice(symbles),random.choice(symbles)]
+        print('|'.join(oofu))
         print('|'.join(oof))
-        if oof[0] == oof[1] == oof[2] =='💎':
+        print('|'.join(oofd))
+        if oof[0] == oof[1] == oof[2] == oofu[0] == oofu[1] == oofu[2] == oofd[0] == oofd[1] == oofd[2]== '💎':
             print('You won le thgy!')
-            credits += 100*int(bet)
+            credits += 100000*int(bet)
             number_of_plays += 1
             print('You now have ' + str(credits) + ' credits')
+            print('__________________')
+            creditswon += 100000*int(bet)
         elif oof[0] == oof[1] == oof[2]:
             print('You won!')
-            credits += 2*int(bet)
+            credits += 3*int(bet)
+            number_of_plays += 1
+            creditswon += 3*int(bet)
+            print('You now have ' + str(credits) + ' credits')
+            print('__________________')
+        elif oofu[0] == oofu[1] == oofu[2]:
+            print('You won!')
+            credits += 3*int(bet)
             number_of_plays += 1
             print('You now have ' + str(credits) + ' credits')
+            print('__________________')
+            creditswon += 3*int(bet)
+        elif oofd[0] == oofd[1] == oofd[2]:
+            print('You won!')
+            credits += 3*int(bet)
+            number_of_plays += 1
+            print('You now have ' + str(credits) + ' credits')
+            print('__________________')
+            creditswon += 3*int(bet)
+        
         else: 
             print('You lost!')
             credits -= int(bet)
             number_of_plays += 1 
             print('You now have ' + str(credits) + ' credits')
     play()
+    
     while True:
-        play()
-        if credits <= 0:
-            print('You have no credits left to play')
-            print('You played ' + str(number_of_plays) + ' times')
-            quit()
+
+        again = input("Do you want to play? (yes/y/no/n): ").lower()
+        
+        if again == "yes" or again == "y":
+            play()
+        elif again == "no" or again == "n":
+            print("Thanks for playing!")
+            break
         else:
-            again = input('Do you want to play again? (yes/y/no/n): ')
-            if again != 'yes' :
-                print('Thanks for playing!')
-                quit()
-       
+            print("Please enter yes or no.")
     
 
                
